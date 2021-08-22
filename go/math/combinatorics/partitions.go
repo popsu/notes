629db70f7090564322404cc6f255495dbd13296c
Tommi Hovi <tommi.hovi@gmail.com>
2021-08-22 23:56:22 +0300

    [go] Use math/big in partition function

--- go/math/combinatorics/partitions.go
+++ go/math/combinatorics/partitions.go
@@ -1,30 +1,33 @@
 package combinatorics
 
+import "math/big"
+
 type twoInts struct {
 	k, n int
 }
 
 type PartitionSolver struct {
-	cache map[twoInts]int
+	cache map[twoInts]*big.Int
 }
 
 func NewPartitionSolver() *PartitionSolver {
-	return &PartitionSolver{cache: make(map[twoInts]int)}
+	return &PartitionSolver{cache: make(map[twoInts]*big.Int)}
 }
 
 // Partitions returns the number of partitions an integer has.
 // https://en.wikipedia.org/wiki/Partition_function_(number_theory)
-func (ps *PartitionSolver) Partitions(n int) int {
+func (ps *PartitionSolver) Partitions(n int) *big.Int {
 	if n <= 1 {
-		return 1
+		return big.NewInt(1)
 	}
 
-	res := 0
+	res := big.NewInt(0)
 
 	// n can be partitioned into exactly 1, 2, 3, ..., n parts
 	// so the result is p(n, 1) + p(n, 2) + p(n, 3) + ... + p(n, n)
 	for i := 1; i <= n; i++ {
-		res += ps.partition(n, i)
+		val := ps.partition(n, i)
+		res.Add(res, val)
 	}
 
 	return res
@@ -35,15 +38,15 @@ func (ps *PartitionSolver) Partitions(n int) int {
 // https://www.mathpages.com/home/kmath091.htm
 // recurrence relation
 // p(n, k) = p(n - 1, k - 1) + p(n -k, k)
-func (ps *PartitionSolver) partition(n, k int) int {
+func (ps *PartitionSolver) partition(n, k int) *big.Int {
 	// one number or all ones, e.g. 4 or 1+1+1+1
 	if k == 1 || k == n {
-		return 1
+		return big.NewInt(1)
 	}
 
 	// e.g. n = 3, k = 4: how many ways we can partition 3 into 4 numbers
 	if n < k {
-		return 0
+		return big.NewInt(0)
 	}
 
 	nk := twoInts{n, k}
@@ -54,7 +57,8 @@ func (ps *PartitionSolver) partition(n, k int) int {
 	}
 
 	// not in cache -> calculate using recurrence and add to cache
-	val := ps.partition(n-1, k-1) + ps.partition(n-k, k)
+	x, y := ps.partition(n-1, k-1), ps.partition(n-k, k)
+	val := big.NewInt(0).Add(x, y)
 
 	ps.cache[nk] = val
 
--- go/math/combinatorics/partitions_test.go
+++ go/math/combinatorics/partitions_test.go
@@ -1,7 +1,9 @@
 package combinatorics
 
 import (
+	"math/big"
 	"strconv"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -9,39 +11,50 @@ import (
 
 func TestPartitionFunction(t *testing.T) {
 	testCases := []struct {
-		in   int
-		want int
+		in         int
+		want       *big.Int
+		wantString string
 	}{
-		{in: 0, want: 1},
-		{in: 1, want: 1},
-		{in: 2, want: 2},
-		{in: 3, want: 3},
-		{in: 4, want: 5},
-		{in: 5, want: 7},
-		{in: 6, want: 11},
-		{in: 7, want: 15},
-		{in: 8, want: 22},
-		{in: 9, want: 30},
-		{in: 10, want: 42},
-		{in: 11, want: 56},
-		{in: 12, want: 77},
-		{in: 13, want: 101},
-		{in: 14, want: 135},
-		{in: 15, want: 176},
-		{in: 16, want: 231},
-		{in: 17, want: 297},
-		{in: 18, want: 385},
-		{in: 19, want: 490},
-		{in: 20, want: 627},
-		{in: 21, want: 792},
-		{in: 22, want: 1002},
-		{in: 100, want: 190_569_292},
+		{in: 0, want: big.NewInt(1)},
+		{in: 1, want: big.NewInt(1)},
+		{in: 2, want: big.NewInt(2)},
+		{in: 3, want: big.NewInt(3)},
+		{in: 4, want: big.NewInt(5)},
+		{in: 5, want: big.NewInt(7)},
+		{in: 6, want: big.NewInt(11)},
+		{in: 7, want: big.NewInt(15)},
+		{in: 8, want: big.NewInt(22)},
+		{in: 9, want: big.NewInt(30)},
+		{in: 10, want: big.NewInt(42)},
+		{in: 11, want: big.NewInt(56)},
+		{in: 12, want: big.NewInt(77)},
+		{in: 13, want: big.NewInt(101)},
+		{in: 14, want: big.NewInt(135)},
+		{in: 15, want: big.NewInt(176)},
+		{in: 16, want: big.NewInt(231)},
+		{in: 17, want: big.NewInt(297)},
+		{in: 18, want: big.NewInt(385)},
+		{in: 19, want: big.NewInt(490)},
+		{in: 20, want: big.NewInt(627)},
+		{in: 21, want: big.NewInt(792)},
+		{in: 22, want: big.NewInt(1002)},
+		{in: 100, want: big.NewInt(190_569_292)},
+		{in: 1000, wantString: "24_061_467_864_032_622_473_692_149_727_991"},
+		{in: 10000, wantString: "36167251325636293988820471890953695495016030339315650422081868605887952568754066420592310556052906916435144"},
 	}
 	for _, tt := range testCases {
 		t.Run(strconv.Itoa(tt.in), func(t *testing.T) {
 			solver := NewPartitionSolver()
 			got := solver.Partitions(tt.in)
-			assert.Equal(t, tt.want, got)
+
+			if tt.want != nil {
+				assert.Equal(t, tt.want, got)
+			} else { // use string to set the value since it's too big
+				want := &big.Int{}
+				want.SetString(strings.Replace(tt.wantString, "_", "", -1), 10)
+
+				assert.Equal(t, want, got)
+			}
 		})
 	}
 }
